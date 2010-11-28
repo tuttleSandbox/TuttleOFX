13bc218fabd367d98a0bff624cd7a15646b2927e
Fabien Castan <fabien.castan@free.fr>
2010-11-28 15:19:36 +0100

    photokey: key process, first version

--- applications/photokey/src/main.cpp
+++ applications/photokey/src/main.cpp
@@ -31,27 +31,52 @@ int main( int argc, char** argv )
 		COUT( "__________________________________________________1" );
 
 		Graph g;
-		Graph::Node& read1    = g.createNode( "fr.tuttle.jpegreader" );
-		Graph::Node& bitdepth = g.createNode( "fr.tuttle.bitdepth" );
-		Graph::Node& invert1  = g.createNode( "fr.tuttle.invert" );
-		Graph::Node& write1    = g.createNode( "fr.tuttle.jpegwriter" );
+		Graph::Node& source     = g.createNode( "fr.tuttle.jpegreader" );
+		Graph::Node& cadre      = g.createNode( "fr.tuttle.jpegreader" );
+		Graph::Node& background = g.createNode( "fr.tuttle.jpegreader" );
+		Graph::Node& sourceBd = g.createNode( "fr.tuttle.bitdepth" );
+		Graph::Node& cadreBd = g.createNode( "fr.tuttle.bitdepth" );
+		Graph::Node& backgroundBd = g.createNode( "fr.tuttle.bitdepth" );
+		Graph::Node& colorSuppr = g.createNode( "fr.tuttle.duranduboi.colorsuppress" );
+		Graph::Node& invertAlpha  = g.createNode( "fr.tuttle.invert" );
+		Graph::Node& merge  = g.createNode( "fr.tuttle.merge" );
+		Graph::Node& write    = g.createNode( "fr.tuttle.jpegwriter" );
 
 		COUT( "__________________________________________________2" );
 		// Setup parameters
-		read1.getParam( "filename" ).set( "data/input.jpg" );
-		bitdepth.getParam( "outputBitDepth" ).set( 3 );
-		COUT_VAR( bitdepth.getParam( "outputBitDepth" ).getString() );
-		write1.getParam( "filename" ).set( "data/output1.jpg" );
+		source.getParam( "filename" ).set( "data/key/source.jpg" );
+		cadre.getParam( "filename" ).set( "data/key/cadre.jpg" );
+		background.getParam( "filename" ).set( "data/key/background.jpg" );
+
+		sourceBd.getParam( "outputBitDepth" ).set( 3 );
+		cadreBd.getParam( "outputBitDepth" ).set( 3 );
+		backgroundBd.getParam( "outputBitDepth" ).set( 3 );
+		colorSuppr.getParam( "greenRate" ).set( 1.0 );
+		colorSuppr.getParam( "output" ).set( 2 );
+		invertAlpha.getParam( "processR" ).set( false );
+		invertAlpha.getParam( "processG" ).set( false );
+		invertAlpha.getParam( "processB" ).set( false );
+		invertAlpha.getParam( "processA" ).set( true );
+		merge.getParam( "mergingFunction" ).set( 19 );
+		write.getParam( "premult" ).set( false );
+		write.getParam( "filename" ).set( "data/key/output.jpg" );
 
 		COUT( "__________________________________________________3" );
-		g.connect( read1, bitdepth );
-		g.connect( bitdepth, invert1 );
-		g.connect( invert1, write1 );
+		g.connect( source, sourceBd );
+		g.connect( cadre, cadreBd );
+		g.connect( background, backgroundBd );
+
+		g.connect( sourceBd, colorSuppr );
+		g.connect( colorSuppr, invertAlpha );
+		g.connect( invertAlpha, merge.getClip("A") );
+		g.connect( backgroundBd, merge.getClip("B") );
+		g.connect( merge, write );
 
 		COUT( "__________________________________________________4" );
-		std::list<std::string> outputs;
-		outputs.push_back( write1.getName() );
-		g.compute( outputs, 0, 0 );
+//		std::list<std::string> outputs;
+//		outputs.push_back( write.getName() );
+//		g.compute( outputs, 0, 0 );
+		g.compute( write, 0 );
 	}
 	catch( tuttle::exception::Common& e )
 	{
